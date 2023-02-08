af5ebe1f273840ceb2274679a6381bdb90cfc740
Dragula <elegantmorpeh@icloud.com>
2023-02-09 01:58:40 +0300

    add Finish

---
+++ Zadacha_25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+static int Method(int a, int b)
+{
+    int Result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        // Console.WriteLine("Первое число в степени второго равно:" + Result);
+        Result = Result * a;
+    }
+    return Result;
+}
+// Method(2,4);
+System.Console.WriteLine("Введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите степень : ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Method(a,b));
---
+++ Zadacha_27/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+using System;
+
+class GFG
+{
+    static int getSum(int n)
+    {
+        int sum = 0;
+
+        while (n != 0)
+        {
+            sum = sum + n % 10;
+            n = n / 10;
+        }
+
+        return sum;
+    }
+
+    public static void Main()
+    {
+        // int n = 452;
+        // Console.Write(getSum(n));
+
+        Console.Write("Vvedite: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.Write(getSum(n));
+    }
+}
